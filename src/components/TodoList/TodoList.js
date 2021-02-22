7380a1e139fe2514ef9fc1d207e2a36c497d1c75
Denys Kozubskyi <denys.kozubskyi@gmail.com>
2021-02-22 13:11:05 +0200

    сделал рефакторинг кода

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -3,87 +3,34 @@ import React, { Component } from 'react';
 import './TodoList.scss';
 
 class TodoList extends Component {
+  makeMarkup = arr => {
+    return arr.map(todo => {
+      return (
+        <li className={todo.type === 'important' ? 'todo__item todo__item__important' : 'todo__item'} key={todo.id}>
+          <input type="checkbox" id={todo.id} checked={todo.completed} />
+          <label htmlFor={todo.id} onClick={() => this.props.onTodoClick(todo.id)}>
+            {todo.text}
+          </label>
+          <button type="button" className="edit-todo"></button>
+          <button type="button" className="delete-todo" onClick={() => this.props.onTodoDelete(todo.id)}></button>
+        </li>
+      );
+    });
+  };
+
   render() {
+    const importantTodos = this.props.todos.filter(todo => todo.type === 'important' && !todo.completed);
+    const standartTodos = this.props.todos.filter(todo => todo.type === 'standart' && !todo.completed);
+    const completedTodos = this.props.todos.filter(todo => todo.completed);
+
     return (
       <>
         <div className="line"></div>
         <ul className="todo__list">
-          {/* {this.props.todos.map(todo => {
-            return (
-              <li
-                className="todo__item"
-                style={{ backgroundColor: todo.type === 'important' && 'rgb(255, 175, 175)' }}
-                key={todo.id}
-              >
-                <input type="checkbox" id={todo.id} checked={todo.completed} />
-                <label htmlFor={todo.id} onClick={() => this.props.onTodoClick(todo.id)}>
-                  {todo.text}
-                </label>
-                <button type="button" className="edit-todo"></button>
-                <button
-                  type="button"
-                  className="delete-todo"
-                  onClick={() => this.props.onTodoDelete(todo.id)}
-                ></button>
-              </li>
-            );
-          })} */}
-          {this.props.todos
-            .filter(todo => todo.type === 'important' && !todo.completed)
-            .map(todo => {
-              return (
-                <li className="todo__item todo__item__important" key={todo.id}>
-                  <input type="checkbox" id={todo.id} checked={todo.completed} />
-                  <label htmlFor={todo.id} onClick={() => this.props.onTodoClick(todo.id)}>
-                    {todo.text}
-                  </label>
-                  <button type="button" className="edit-todo"></button>
-                  <button
-                    type="button"
-                    className="delete-todo"
-                    onClick={() => this.props.onTodoDelete(todo.id)}
-                  ></button>
-                </li>
-              );
-            })}
-          {/* <div className="line"></div> */}
-          {this.props.todos
-            .filter(todo => todo.type === 'standart' && !todo.completed)
-            .map(todo => {
-              return (
-                <li className="todo__item" key={todo.id}>
-                  <input type="checkbox" id={todo.id} checked={todo.completed} />
-                  <label htmlFor={todo.id} onClick={() => this.props.onTodoClick(todo.id)}>
-                    {todo.text}
-                  </label>
-                  <button type="button" className="edit-todo"></button>
-                  <button
-                    type="button"
-                    className="delete-todo"
-                    onClick={() => this.props.onTodoDelete(todo.id)}
-                  ></button>
-                </li>
-              );
-            })}
-          {/* <div className="line"></div> */}
-          {this.props.todos
-            .filter(todo => todo.completed)
-            .map(todo => {
-              return (
-                <li className="todo__item" key={todo.id}>
-                  <input type="checkbox" id={todo.id} checked={todo.completed} />
-                  <label htmlFor={todo.id} onClick={() => this.props.onTodoClick(todo.id)}>
-                    {todo.text}
-                  </label>
-                  <button type="button" className="edit-todo"></button>
-                  <button
-                    type="button"
-                    className="delete-todo"
-                    onClick={() => this.props.onTodoDelete(todo.id)}
-                  ></button>
-                </li>
-              );
-            })}
+          {/* {this.makeMarkup(this.props.todos)} */}
+          {this.makeMarkup(importantTodos)}
+          {this.makeMarkup(standartTodos)}
+          {this.makeMarkup(completedTodos)}
         </ul>
       </>
     );
