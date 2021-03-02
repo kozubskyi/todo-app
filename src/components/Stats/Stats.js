51386edd011d650934422cc56ad151e9ff875283
Denys Kozubskyi <denys.kozubskyi@gmail.com>
2021-03-02 09:52:12 +0200

    сделал мелкие правки и пофиксил баги

--- src/components/Stats/Stats.js
+++ src/components/Stats/Stats.js
@@ -8,12 +8,11 @@ const Stats = ({ todos }) => {
       <div className="stats">
         <button className="stats__btn stats__all-todos">{todos.length}</button>
         <button className="stats__btn stats__undone-todos">{todos.filter(todo => !todo.completed).length}</button>
-        <button className="stats__btn stats__important-todos">
+        {/* <button className="stats__btn stats__important-todos">
           {todos.filter(todo => todo.type === 'important' && !todo.completed).length}
-        </button>
+        </button> */}
         <button className="stats__btn stats__done-todos">{todos.filter(todo => todo.completed).length}</button>
       </div>
-      <div className="line"></div>
     </>
   );
 };
