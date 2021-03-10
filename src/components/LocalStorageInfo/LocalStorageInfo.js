83d168df6bb6430efad856b17d80c697ebed1a72
Denys Kozubskyi <denys.kozubskyi@gmail.com>
2021-03-10 10:56:52 +0200

    внес мелкие правки

--- src/components/LocalStorageInfo/LocalStorageInfo.js
+++ src/components/LocalStorageInfo/LocalStorageInfo.js
@@ -4,9 +4,9 @@ import './LocalStorageInfo.scss'
 const LocalStorageInfo = () => {
   return (
     <p className="localStorage__text">
-      Для хранения данных <span className="localStorage__text active">не используется бекенд!</span> Все данные хранятся
+      Для хранения данных <span className="localStorage__text__active">не используется бекенд!</span> Все данные хранятся
       в локальном хранилище браузера - Local Storage.{' '}
-      <span className="localStorage__text active">Не очищайте Local Storage</span> во избежание удаления всех todos.
+      <span className="localStorage__text__active">Не очищайте Local Storage</span> во избежание удаления всех todos.
     </p>
   );
 };
